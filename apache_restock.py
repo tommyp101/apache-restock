d4bd526954c239d631bae15bfe6ee571e9bd1d0c
tommyp101 <44925554+tommyp101@users.noreply.github.com>
2018-12-15 23:22:02 +0100

    Update apache_restock.py

--- apache_restock.py
+++ apache_restock.py
@@ -45,7 +45,7 @@ supreme = [
 
 
 
-
+dd
 site_links = {
     "nike": [],
     "adidas": [],
@@ -330,53 +330,53 @@ print(webscrape_data["headers"]["nike"]["cookie"])
 
 
 
-for index, nike_link in enumerate(site_links["nike"]):
-    stock_list = get_nike_link_stock(nike_link["link"])
+# for index, nike_link in enumerate(site_links["nike"]):
+#     stock_list = get_nike_link_stock(nike_link["link"])
 
-    if stock_list["item_in_stock"] == False:
-        site_links["nike"][index]["last_in_stock"] = False
-    else:
-        site_links["nike"][index]["last_in_stock"] = True
-        if nike_link["wanted_sizes"] == []:
-            site_links["nike"][index]["last_in_stock_sizes"] = stock_list["in_stock"]
-        else:
-            for size in nike_link["wanted_sizes"]:
-                if size in stock_list["in_stock"]:
-                    site_links["nike"][index]["last_in_stock_sizes"].append(size)
-
-
-
-
-for index, adidas_link in enumerate(site_links["adidas"]):
-    stock_list = get_adidas_link_stock(adidas_link["adidas_item_id"])
-
-    if stock_list["item_in_stock"] == False:
-        site_links["adidas"][index]["last_in_stock"] = False
-    else:
-        site_links["adidas"][index]["last_in_stock"] = True
-        if adidas_link["wanted_sizes"] == []:
-            site_links["adidas"][index]["last_in_stock_sizes"] = stock_list["in_stock"]
-        else:
-            for size in adidas_link["wanted_sizes"]:
-                if size in stock_list["in_stock"]:
-                    site_links["adidas"][index]["last_in_stock_sizes"].append(size)
+#     if stock_list["item_in_stock"] == False:
+#         site_links["nike"][index]["last_in_stock"] = False
+#     else:
+#         site_links["nike"][index]["last_in_stock"] = True
+#         if nike_link["wanted_sizes"] == []:
+#             site_links["nike"][index]["last_in_stock_sizes"] = stock_list["in_stock"]
+#         else:
+#             for size in nike_link["wanted_sizes"]:
+#                 if size in stock_list["in_stock"]:
+#                     site_links["nike"][index]["last_in_stock_sizes"].append(size)
 
 
 
 
-for index, supreme_link in enumerate(site_links["supreme"]):
-    stock_list = get_supreme_link_stock(supreme_link["link"])
+# for index, adidas_link in enumerate(site_links["adidas"]):
+#     stock_list = get_adidas_link_stock(adidas_link["adidas_item_id"])
 
-    if stock_list["item_in_stock"] == False:
-        site_links["supreme"][index]["last_in_stock"] = False
-    else:
-        site_links["supreme"][index]["last_in_stock"] = True
-        if supreme_link["wanted_sizes"] == []:
-            site_links["supreme"][index]["last_in_stock_sizes"] = stock_list["in_stock"]
-        else:
-            for size in supreme_link["wanted_sizes"]:
-                if size in stock_list["in_stock"]:
-                    site_links["supreme"][index]["last_in_stock_sizes"].append(size)
+#     if stock_list["item_in_stock"] == False:
+#         site_links["adidas"][index]["last_in_stock"] = False
+#     else:
+#         site_links["adidas"][index]["last_in_stock"] = True
+#         if adidas_link["wanted_sizes"] == []:
+#             site_links["adidas"][index]["last_in_stock_sizes"] = stock_list["in_stock"]
+#         else:
+#             for size in adidas_link["wanted_sizes"]:
+#                 if size in stock_list["in_stock"]:
+#                     site_links["adidas"][index]["last_in_stock_sizes"].append(size)
+
+
+
+
+# for index, supreme_link in enumerate(site_links["supreme"]):
+#     stock_list = get_supreme_link_stock(supreme_link["link"])
+
+#     if stock_list["item_in_stock"] == False:
+#         site_links["supreme"][index]["last_in_stock"] = False
+#     else:
+#         site_links["supreme"][index]["last_in_stock"] = True
+#         if supreme_link["wanted_sizes"] == []:
+#             site_links["supreme"][index]["last_in_stock_sizes"] = stock_list["in_stock"]
+#         else:
+#             for size in supreme_link["wanted_sizes"]:
+#                 if size in stock_list["in_stock"]:
+#                     site_links["supreme"][index]["last_in_stock_sizes"].append(size)
 
 
 
